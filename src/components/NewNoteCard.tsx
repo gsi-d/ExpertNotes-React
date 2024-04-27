b1b97f7ad719c0fe738cb3785a7746f562ff8121
Guilherme <you@example.comguimota_silver@hotmail.com>
2024-04-27 15:41:15 -0300

    Correção do botão close

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -87,6 +87,12 @@ export function NewNoteCard({ onNewNoteCreated }: NewNoteCardProps){
       }
     }
 
+    function handleClickClose(){
+      setValue("");
+      setShouldShowOnboarding(true)
+      setIsRecording(false)
+    }
+
     return(
       <Dialog.Root>
         <Dialog.Trigger className="rounded-md bg-slate-700 p-5 flex flex-col gap-3 text-left overflow-hidden hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -96,7 +102,7 @@ export function NewNoteCard({ onNewNoteCreated }: NewNoteCardProps){
         <Dialog.Portal>
           <Dialog.Overlay className="inset-0 fixed bg-black/50"/>
           <Dialog.Content className="z-10 fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none">
-            <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <Dialog.Close onClick={handleClickClose} className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
               <X className="size-5"/>
             </Dialog.Close>
             <form onSubmit={(e) => handleSaveNote(e)} className="flex flex-1 flex-col">
@@ -120,7 +126,7 @@ export function NewNoteCard({ onNewNoteCreated }: NewNoteCardProps){
                   Gravando! (clique p/ interromper)
                 </button>
               :
-                <button onClick={(e) => handleSaveNote(e)} type="button" className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-bold hover:bg-lime-500">
+                <button disabled={value == ""} onClick={(e) => handleSaveNote(e)} type="button" className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-bold hover:bg-lime-500">
                   Salvar nota
                 </button>
               }
